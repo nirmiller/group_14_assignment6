6120c365c0658c9b7da808b8c261795c5f2a127b
meagan8 <hennigmt@gmail.com>
2026-03-29 13:13:32 -0500

    firework particle class

--- group14_assignment6/Game1.cs
+++ group14_assignment6/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System;
 
 namespace group14_assignment6;
 
